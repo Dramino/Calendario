0e906cd87a93c1c64dae5d0dfa3f24a614817bae
Isaac Rosales Garcia <isaacrosales@gmail.com>
2020-09-29 14:33:08 -0500

    Description button added

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,6 @@
 let modal = document.getElementById("miModal");
 let daysContainer = document.getElementsByClassName("calendarContainer");
 
-const addOnClicEvent = (object) => {
-  let day = object.textContent;
-  object.onclick = (e) => displayModal(day);
-};
-
 const displayModal = (day) => {
   modal.style.display = "block";
   modal.getElementsByTagName("p")[0].innerHTML = day;
@@ -13,6 +8,13 @@ const displayModal = (day) => {
 
 const closeModal = () => {
   modal.style.display = "none";
+};
+
+const addOnClicEvent = (object) => {
+  let day = object.textContent;
+  console.log(day[0]);
+  let texto = day + " de Junio de 2020";
+  object.onclick = (e) => displayModal(texto);
 };
 
 for (const el of daysContainer) {
